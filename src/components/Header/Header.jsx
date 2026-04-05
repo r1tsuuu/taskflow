46ba7871b31ec85172aa45d29dd6f41b05f9c657
Ritsu- <biasonneilaldous@gmail.com>
2026-04-05 11:57:10 +0800

    [Header] build header UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
-function Header() {
-  return <div className="header"></div>;
+import './Header.css';
+
+function Header({ onAddTask, onAddMember }) {
+  return (
+    <header className="header">
+      <h1 className="header__title">TaskFlow</h1>
+      <div className="header__actions">
+        <button className="header__btn header__btn--member" onClick={onAddMember}>
+          + Add Member
+        </button>
+        <button className="header__btn header__btn--task" onClick={onAddTask}>
+          + Add Task
+        </button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
